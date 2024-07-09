a8c8911aa3cf84d7d592e043e478e0186d2bf5fc
leezy <leezy016@foxmail.com>
2024-07-09 11:43:11 +0800

    1st commit

---
+++ Source/Min/Min.Build.cs
@@ -0,0 +1,11 @@
+using UnrealBuildTool;
+
+public class Min : ModuleRules
+{
+	public Min(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+	}
+}
---
+++ Source/Min/Min.cpp
@@ -0,0 +1,4 @@
+#include "Min.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Min, "Min" );
